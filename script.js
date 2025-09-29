86fb5ff6194822a42efc256341ca2b1c6c2781c0
Ndahai Arenas <gumojammbo@gmail.com>
2025-09-29 11:16:31 -0600

    sej

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ const secretVideo = document.getElementById("secret-video");
 const closeEgg = document.getElementById("close-egg");
 
 const secretCode = "tlohtzin";
+const sejCode = "sej";
 
 function showVideo() {
     const input = secretInput.value.toLowerCase();
-    if (input === secretCode) {
-        secretVideo.src = "https://www.youtube.com/embed/19VUidcK24Q?si=JQQgzl3cUrC8saVX&autoplay=1";
+
+    if (input === sejCode) {
+        // Segundo easter egg
+        secretVideo.src = "https://www.youtube.com/embed/-BoMd8De640?si=6QcQ1k6HLL_czykL&amp;start=381"; // puedes poner otro video
         easterEgg.style.display = "block";
         secretInput.value = "";
     } else {
